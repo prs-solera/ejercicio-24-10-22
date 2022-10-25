026249c49889a9e56a218a4515ca050804a6550c
Pablo Rodríguez Saseta <Pablo.RodriguezSaseta@solera.com>
2022-10-25 16:07:45 +0200

    Web config

---
+++ src/main/java/com/questionnaire/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.questionnaire.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+// Copiado y pegado íntegramente de internet
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+        //registry.addMapping("/api/boolq").allowCredentials(true).allowedOrigins("http://localhost:3000");
+    }
+}
